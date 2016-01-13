a678d376e0f67d860682721a68de3f3236626f01
shamoanjac <erchuckyprimo@gmail.com>
2016-01-13 19:30:46 +0100

    Added some comments and improved tests on posts

--- src/tests/posts.c
+++ src/tests/posts.c
@@ -10,16 +10,22 @@ int test_create_post(void* data)
     (void) data;
     int status = 1;
     Post *post = new_post("title", "name", "text", 1, 2);
+    /* Make sure fields have been set correctly */
     assert(strcmp(post->title, "title") == 0);
     assert(strcmp(post->name, "name") == 0);
     assert(strcmp(post->txt, "text") == 0);
     assert(post->id == 1);
     assert(post->reply_to == 2);
+    /* Make sure fields have been guessed correctly */
     assert(! post->is_op);
     free_post(post);
     /* Test default name */
-    post = new_post("title", NULL, "text", 0, 0);
+    post = new_post("title", NULL, "text", 1, 2);
     assert(strcmp(post->name, "Anonymous") == 0);
+    assert(strcmp(post->title, "title") == 0);
+    assert(strcmp(post->txt, "text") == 0);
+    assert(post->id == 1);
+    assert(post->reply_to == 2);
     free_post(post);
     return status;
 }
