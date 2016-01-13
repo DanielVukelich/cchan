d2522ed57168a50b1648dba7c9218a31e33b09d7
shamoanjac <erchuckyprimo@gmail.com>
2016-01-13 19:34:35 +0100

    Put OP post creation tests inside the same function as testing on standard posts

--- src/tests/posts.c
+++ src/tests/posts.c
@@ -26,15 +26,15 @@ int test_create_post(void* data)
     assert(strcmp(post->txt, "text") == 0);
     assert(post->id == 1);
     assert(post->reply_to == 2);
+    assert(! post->is_op);
     free_post(post);
-    return status;
-}
-
-int test_create_OP_post(void *data)
-{
-    (void) data;
-    int status = 1;
-    Post *post = new_post("title", "test", "text", 0, -1);
+    /* Create an OP post */
+    post = new_post("title", "name", "text", 1, -1);
+    assert(strcmp(post->name, "name") == 0);
+    assert(strcmp(post->title, "title") == 0);
+    assert(strcmp(post->txt, "text") == 0);
+    assert(post->id == 1);
+    assert(post->reply_to == -1);
     assert(post->is_op);
     free_post(post);
     return status;
@@ -45,8 +45,6 @@ int test_posts()
     int status = 1;
     puts("Testing creating a post...");
     test_create_post(NULL);
-    puts("Testing creating an OP post...");
-    test_create_OP_post(NULL);
     return status;
 }
 
