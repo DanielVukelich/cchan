0482c96f3111dcc689aa0b094efdab8d933d5ff6
shamoanjac <erchuckyprimo@gmail.com>
2016-01-16 06:38:22 +0100

    Added tests for rendering

--- src/tests/main.c
+++ src/tests/main.c
@@ -1,6 +1,7 @@
 #include "threads.c"
 #include "posts.c"
 #include "boards.c"
+#include "rendering.c"
 
 int main(int argc, char **argv) {
     (void) argc;
@@ -8,6 +9,7 @@ int main(int argc, char **argv) {
     test_posts();
     run_thread_tests();
     test_boards();
+    test_rendering();
     return 0;
 }
 
